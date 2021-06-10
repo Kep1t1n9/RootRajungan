3b8990f1f678f6aa0bb75983f82e7189413c721c
K3p1t1n9 R4jun94n <85659549+Kep1t1n9@users.noreply.github.com>
2021-06-11 01:40:32 +0700

    Add files via upload

---
+++ RootRajungan.py
@@ -0,0 +1,19 @@
+#!/bin/python
+import os
+
+
+#punya KEPITING RAJUNGAN mau pelajari silahkan
+#mo ganti,gua ddos lu!enjoyyyy
+os.system("clear")
+os.system("sleep 2")
+
+print ("\033[1;32mAUTHOR :  \033[1;34mKEPITING")
+print ("\033[1;33mYoutube Channel FILHAB G")
+print ("\033[1;35m SILAHKAN DIGUNAKAN,SEMOGA BERMANFAAT")
+print ("\33[0;36m()==============UBUNTU ROOT INSTALL==========()")
+print
+print ("MENGINSTALL UBUNTU.....")
+os.system("sleep 3")
+os.system("pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Ubuntu/ubuntu.sh")
+os.system("bash ubuntu.sh")
+os.system("sleep 2")
